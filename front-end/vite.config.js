729b2c466be282b062c1393a46c66b976366d9e0
Nathaniel Mitchell <deltaxy@Nathaniels-MacBook-Air.local>
2025-07-13 19:05:41 -0500

    Add env vars to vite

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
+  define: {
+    'process.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify(process.env.VITE_GOOGLE_CLIENT_ID || ''),
+    },
   plugins: [react()],
   build: {
     outDir: 'dist',
