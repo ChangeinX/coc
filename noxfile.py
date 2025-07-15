accfec65d00f80b6a4f5c5980b2dae61d4b0d54c
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-07-15 16:21:56 -0500

    Add front-end test suite (#71)

--- noxfile.py
+++ noxfile.py
@@ -12,3 +12,6 @@ def tests(session: nox.Session) -> None:
     session.install("-r", "back-end/requirements.txt")
     session.install("-r", "sync/requirements.txt")
     session.run("pytest", "-q")
+    session.run("npm", "--prefix", "front-end", "install", external=True)
+    session.run("npm", "--prefix", "front-end", "run", "test", external=True)
+    session.run("npm", "--prefix", "front-end", "run", "build", external=True)
