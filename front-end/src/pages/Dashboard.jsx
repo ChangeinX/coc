1d72c2728707e72c18a0dc97e5e29dc3e22fa5d4
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-07-15 16:11:40 -0500

    Revert "fix mobile member card expansion (#69)" (#70)
    This reverts commit 61bc374121135454ed2339019af73c81ecefcb35.

--- front-end/src/pages/Dashboard.jsx
+++ front-end/src/pages/Dashboard.jsx
@@ -386,9 +386,9 @@ export default function Dashboard({ defaultTag, showSearchForm = true, onClanLoa
                             {activeTab === 'top' && (
                                 <div
                                     className="bg-white rounded shadow ring-2 ring-rose-200"
-                                    style={{ height: listHeight }}
+                                    style={{ height: Math.min(listHeight, topRisk.length * 60) }}
                                 >
-                                    <MemberAccordionList members={topRisk} height={listHeight} />
+                                    <MemberAccordionList members={topRisk} height={Math.min(listHeight, topRisk.length * 60)} />
                                 </div>
                             )}
                             {activeTab === 'all' && (
