dec796637260e5d19fc4fafef027ded82a21914b
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-07-15 15:21:33 -0500

    feat(front-end): proxy api for dev (#68)

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
+  server: process.env.VITE_API_URL
+    ? {
+        proxy: {
+          '/api': {
+            target: process.env.VITE_API_URL,
+            changeOrigin: true,
+          },
+        },
+      }
+    : undefined,
   build: {
     outDir: 'dist',
     sourcemap: false,
