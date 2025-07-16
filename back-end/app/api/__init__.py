103c11a5dd9cafa2d8a7c8eb2d8cc681142c19bf
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-07-16 02:16:24 -0500

    Fix asset proxy helper

--- back-end/app/api/__init__.py
+++ back-end/app/api/__init__.py
@@ -8,6 +8,7 @@ from .player_routes import bp as player_bp
 from .war_routes import bp as war_bp
 from .health_routes import bp as health_bp
 from .user_routes import bp as user_bp
+from .asset_routes import bp as asset_bp
 
 
 def register_blueprints(app: Flask):
@@ -16,3 +17,4 @@ def register_blueprints(app: Flask):
     app.register_blueprint(war_bp)
     app.register_blueprint(health_bp)
     app.register_blueprint(user_bp)
+    app.register_blueprint(asset_bp)
