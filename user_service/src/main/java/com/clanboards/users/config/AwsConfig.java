cec1814d461a6f588d67cccc0971f91ce0d23c04
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-07-23 20:06:53 -0500

    feat: fetch CORS origins from AWS

--- user_service/src/main/java/com/clanboards/users/config/AwsConfig.java
+++ user_service/src/main/java/com/clanboards/users/config/AwsConfig.java
@@ -6,12 +6,20 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 
 @Configuration
 public class AwsConfig {
     @Bean
     public DynamoDbClient dynamoDbClient(@Value("${aws.region:us-east-1}") String region) {
         return DynamoDbClient.builder()
+                .region(Region.of(region))
+                .build();
+    }
+
+    @Bean
+    public SecretsManagerClient secretsManagerClient(@Value("${aws.region:us-east-1}") String region) {
+        return SecretsManagerClient.builder()
                 .region(Region.of(region))
                 .build();
     }
