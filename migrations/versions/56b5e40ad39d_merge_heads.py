cc02f4d6191157f77ca9f248fe90ecb3340b0430
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-07-24 00:40:03 -0500

    Add merged heads and missing health controller (#275)

---
+++ migrations/versions/56b5e40ad39d_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 56b5e40ad39d
+Revises: 4ed621612137, cad1bc3dd940
+Create Date: 2025-07-23 15:48:57.755713
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '56b5e40ad39d'
+down_revision = ('4ed621612137', 'cad1bc3dd940')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
