05fc3ccbaffc7c3914fd133ac6a8645df6d1a21a
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-07-23 19:37:38 -0500

    fix: loosen CORS for spring services (#271)
    * fix: loosen CORS for spring services
    
    * ci: retrigger

--- user_service/src/main/java/com/clanboards/users/config/CorsConfig.java
+++ user_service/src/main/java/com/clanboards/users/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.clanboards.messages.config;
+package com.clanboards.users.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
