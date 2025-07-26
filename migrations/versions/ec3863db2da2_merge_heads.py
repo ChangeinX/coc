8405a0e5a0dad08137a70a2a3804de8d15b14d62
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-07-26 13:31:04 -0500

    migrations (#340)
    * add merge heads and gradlew
    
    * update .gitignore

---
+++ migrations/versions/ec3863db2da2_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: ec3863db2da2
+Revises: 56b5e40ad39d, ba5f1bc1f1aa
+Create Date: 2025-07-26 13:13:48.658960
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ec3863db2da2'
+down_revision = ('56b5e40ad39d', 'ba5f1bc1f1aa')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
