516d3ab8bdfd3badf56f34f812fcc4582efbd85e
ChangeinX <userdata0@gmail.com>
2025-07-26 20:49:32 -0500

    register bc early

--- notifications/src/main/java/com/clanboards/notifications/NotificationsApplication.java
+++ notifications/src/main/java/com/clanboards/notifications/NotificationsApplication.java
@@ -2,10 +2,15 @@ package com.clanboards.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.Security;
 
 @SpringBootApplication
 public class NotificationsApplication {
     public static void main(String[] args) {
+        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
         SpringApplication.run(NotificationsApplication.class, args);
     }
 }
