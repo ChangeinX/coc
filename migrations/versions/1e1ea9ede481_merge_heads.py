ea1dd77882e8d6820e32b1afaec1d6d7ca7f8e54
ChangeinX <userdata0@gmail.com>
2025-07-27 18:46:27 -0500

    Merge db heads and drop .gradle

---
+++ migrations/versions/1e1ea9ede481_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 1e1ea9ede481
+Revises: 623ae58d08b1, ec3863db2da2
+Create Date: 2025-07-27 18:42:16.495104
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1e1ea9ede481'
+down_revision = ('623ae58d08b1', 'ec3863db2da2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
