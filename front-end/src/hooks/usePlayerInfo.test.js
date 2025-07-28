1b8da36fd7b5d65ea6e9d03a5ede8445347c0ba3
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-07-28 14:05:25 -0500

    feat(front-end): cache clan and player info

---
+++ front-end/src/hooks/usePlayerInfo.test.js
@@ -0,0 +1,7 @@
+import usePlayerInfo from './usePlayerInfo.js';
+
+describe('usePlayerInfo hook', () => {
+  it('exports a function', () => {
+    expect(typeof usePlayerInfo).toBe('function');
+  });
+});
