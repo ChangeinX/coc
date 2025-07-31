cdb8f25ff5fe3817d30ecd8a863a7a8bf8552403
ChangeinX <userdata0@gmail.com>
2025-07-30 20:22:12 -0500

    Merge migration heads

---
+++ migrations/versions/6b1f0826cb5a_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: 6b1f0826cb5a
+Revises: d9ddc8754765, f901db3f1df2
+Create Date: 2025-07-30 20:21:42.131377
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6b1f0826cb5a'
+down_revision = ('d9ddc8754765', 'f901db3f1df2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
