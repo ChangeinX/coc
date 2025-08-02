079854ecd0b12c6b1bce236c8e48ef619df78247
ChangeinX <84469475+ChangeinX@users.noreply.github.com>
2025-08-02 17:30:22 -0500

    feat: enhance chat moderation and spam controls

--- messages-java/src/main/java/com/clanboards/messages/repository/ModerationRepository.java
+++ messages-java/src/main/java/com/clanboards/messages/repository/ModerationRepository.java
@@ -3,4 +3,7 @@ package com.clanboards.messages.repository;
 import com.clanboards.messages.model.ModerationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ModerationRepository extends JpaRepository<ModerationRecord, Long> {}
+public interface ModerationRepository extends JpaRepository<ModerationRecord, Long> {
+
+  long countByUserId(String userId);
+}
