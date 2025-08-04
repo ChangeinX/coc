929cb306edfe461d30b0b37c05de5095cfe7d477
ChangeinX <userdata0@gmail.com>
2025-08-03 19:59:02 -0500

    merge split heads

---
+++ migrations/versions/36c3d8a839b0_new_heads.py
@@ -0,0 +1,24 @@
+"""new heads
+
+Revision ID: 36c3d8a839b0
+Revises: 2473f333fc90, 2bb376b11a5b
+Create Date: 2025-08-03 00:03:30.186332
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '36c3d8a839b0'
+down_revision = ('2473f333fc90', '2bb376b11a5b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/ff0393be780b_diff_heads.py
@@ -0,0 +1,24 @@
+"""diff heads
+
+Revision ID: ff0393be780b
+Revises: 36c3d8a839b0, b3ab9e12ec82
+Create Date: 2025-08-03 00:04:16.438415
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ff0393be780b'
+down_revision = ('36c3d8a839b0', 'b3ab9e12ec82')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
